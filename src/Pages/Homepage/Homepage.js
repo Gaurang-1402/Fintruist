7dafff45e23bc3a5051124b34896d2c19a0aec1f
Gaurang Ruparelia <gr2159@nyu.edu>
2022-04-02 01:50:56 -0400

    complete sidenav and homepage

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,32 +1,32 @@
 import React from "react"
 import logo from "../../assets/images/logo.png"
+import background from "../../assets/images/background.png"
+import { useNavigate } from "react-router-dom"
 const Homepage = () => {
-  return (
-    <div className='h-screen bg-cust_back'>
-      <div style={{ paddingTop: "10rem" }}>
-        <div
-          style={{ marginLeft: "15rem" }}
-          className='h-144 w-144 bg-slate-400'
-        >
-          <img src={logo} className='' alt='logo' />
+  //   const navigate = useNavigate()
 
+  return (
+    <div className='h-screen w-full'>
+      <div style={{ paddingTop: "10rem" }}>
+        <div style={{ marginLeft: "6rem" }} className='h-184 w-184'>
+          <img src={logo} className='' alt='logo' />
           <div className='text-left ml-10 pt-5 text-7xl font-bold'>Trustly</div>
           <div className='flex pt-10 w-full h-20'>
             <button
               className='text-center bg-cust_tertiary ml-10
-           text-1xl text-white font-bold w-56 h-15 rounded-xl'
+              text-2xl text-white font-bold w-60 h-20 rounded-xl'
             >
               Buyer
             </button>
             <button
               className='text-center bg-cust_tertiary ml-10
-           text-1xl text-white font-bold w-56 h-15 rounded-xl'
+              text-2xl text-white font-bold w-60 h-20 rounded-xl'
             >
               Seller
             </button>
             <button
               className='text-center bg-cust_tertiary ml-10
-           text-1xl text-white font-bold w-56 h-15 rounded-xl'
+           text-2xl text-white font-bold w-60 h-20 rounded-xl'
             >
               Campaign
             </button>
