0d039494c482413f52730d5494a9e9dea49eef37
neilblaze <putubanerjee23@gmail.com>
2022-04-02 23:51:14 +0530

    updates :zap:

--- components/CtribForm.js
+++ components/CtribForm.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
 
+import web3 from "../ethereum/web3";
+import Campaign from "../ethereum/campaign";
 
+import { Router } from "../routes";
+// import Layout from "../components/Layout";
 
 class CtribForm extends Component {
   state = {
     value: "", errorMessage: "", loading: false,
   };
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const Campgn = Campgn(this.props.address);
+    // const accounts = await web3.eth.getAccounts();
+    this.setState({ loading: true, errorMessage: "" });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await Campgn.methods.contribute().send({
+        from: accounts[0], value: web3.utils.toWei(this.state.value, "ether"),
+      });
+      // Router .pushRoute(`/campaigns/${this.props.address}/requests`);
+      Router.replaceRoute(`/campaigns/${this.props.address}`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    // this.setState({ loading: false });
+    this.setState({ loading: false, value: "" });
+  };
 
   render() {
     return (
