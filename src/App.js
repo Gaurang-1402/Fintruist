f204bd5294a386eb52c7f20e1f2e99a347b95925
Gaurang Ruparelia <gr2159@nyu.edu>
2022-04-02 12:35:12 -0400

    metamask integration

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Homepage from "./Pages/Homepage/Homepage"
 import SideNav from "./Components/SideNav/SideNav"
-import background from "./assets/images/background.png"
+import CreatePaymentLinkPage from "./Pages/CreatePaymentLinkPage/CreatePaymentLinkPage"
 
+import background from "./assets/images/background.png"
+// import { MoralisProvider } from "react-moralis"
 function App() {
+  // const serverUrl = "https://bqwuyxfazmgl.usemoralis.com:2053/server"
+  // const appId = "8AFd0WfrQKOtx64ZOz7M7tgf3yJu2SD3zAKO5eDM"
   return (
     <>
-      <div
-        className='App flex bg-cust_back'
-        style={{ backgroundImage: `url(${background})` }}
-      >
+      {/* <MoralisProvider serverUrl={serverUrl} appId={appId}> */}
+      <div className='App flex h-screen bg-cust_back'>
         <Router>
           <SideNav />
           <Routes>
             <Route exact path='/' element={<Homepage />} />
+
+            <Route
+              exact
+              path='/createpayment'
+              element={<CreatePaymentLinkPage />}
+            />
           </Routes>
         </Router>
       </div>
+      {/* </MoralisProvider> */}
     </>
   )
 }
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -3,18 +3,17 @@ import logo from "../../assets/images/logo.png"
 import background from "../../assets/images/background.png"
 import { useNavigate } from "react-router-dom"
 import sideStuff from "../../assets/images/sidestuff.png"
-import metamask from "../../assets/images/metamask.png"
+
 const Homepage = () => {
-  //   const navigate = useNavigate()
+  const navigate = useNavigate()
 
   return (
     <div className='h-screen w-full'>
       <div div className='flex w-full justify-end pr-5'>
-        <div className='pt-3'>
-          <img src={metamask} className='w-24 h-24 ' />
-        </div>
+        <div className='pt-3'></div>
         <div className='pt-4  align-right pr-3'>
           <button
+            // onClick={handleCustomLogin}
             className='text-center bg-cust_tertiary ml-10
            text-2xl text-white font-bold w-60 h-20 rounded-xl'
           >
@@ -25,9 +24,7 @@ const Homepage = () => {
       <div style={{ paddingTop: "8rem" }}>
         <div style={{ marginLeft: "6rem" }} className='h-184 w-184'>
           <img src={logo} className='' alt='logo' />
-          <div className='text-left ml-10 pt-5 text-7xl font-bold'>
-            Fintruist{" "}
-          </div>
+
           <div className='flex pt-10 w-full h-20'>
             <button
               className='text-center bg-cust_tertiary ml-10
