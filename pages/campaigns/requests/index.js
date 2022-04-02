f73cfc968a307f6b1c8b7485e886fccebbd9737b
neilblaze <putubanerjee23@gmail.com>
2022-04-03 02:53:29 +0530

    bug fixing

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Lyout from "../../../components/Lyout";
 
-
-import Web3 from "../../../web3";
+/* This is importing the Campaign class from the ethereum folder. */
+import Campaign from "../../../ethereum/campaign";
+// import Web3 from "../../../web3";
 import ReqRow from "../../../components/ReqRow";
 
 class RequestIndex extends Component {
@@ -13,17 +14,17 @@ class RequestIndex extends Component {
     const { address } = props.query;
     const campaign = Campaign(address);
     const requestCnt = await campaign.methods.getReqstCnt().call();
-    const requests = await Promise.all(
-      Array(parseInt(requestCnt)).fill().map((element, index) => {
-        return campaign.methods.requests(index).call();
-      })
+    // const requests = await Promise.all(
+    //   Array(parseInt(requestCnt)).fill().map((element, index) => {
+    //     return campaign.methods.requests(index).call();
+    //   })
     const approverCnt = await campaign.methods.approverCnt().call();
 
     const requests = await Promise.all(
       Array(parseInt(requestCnt))
         .fill()
         .map((element, index) => {
-          console.log(index);
+          // console.log(index);
           return campaign.methods.requests(index).call();
         })
     );
@@ -45,6 +46,7 @@ class RequestIndex extends Component {
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
 
     return (
       <Lyout>
@@ -56,6 +58,19 @@ class RequestIndex extends Component {
             </Button>
           </a>
         </Link>
+
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell><HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell><HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell><HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+          {/* <Table.Footer> */}
+        </Table>
         <div>Found {this.props.requestCount} requests.</div>
       </Lyout>
     );
