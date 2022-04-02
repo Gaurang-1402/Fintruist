127635e8ecebbde1c3cb072780329660c493ccc0
GabrielWeinbrenner <gabriel.weinbrenner@gmail.com>
2022-04-02 02:32:45 -0400

    redesign

--- src/Pages/PaymentResult/PaymentResult.js
+++ src/Pages/PaymentResult/PaymentResult.js
@@ -10,28 +10,36 @@ const PaymentResult = () => {
 
       </div>
 
-      <div className="backgroundCard grid grid-cols-1 gap-2 rounded-2xl">
+      <div className="backgroundCard grid grid-cols-1 rounded-2xl">
         <div className="m-3">
             <h3 className="text-3xl text-left sfloat-left text-white m-4">Share this link with the buyer. Once the buyer deposits tokens into the address, you will be redirected to the next step.</h3>
         </div>
-        <div className="grid grid-col-2  grid-flow-col backgroundSubCard m-3 gap-4 rounded-lg" style={{width: "1000px", height: "150px"}}>
-                <div className="flex flex-wrap mt-8 ml-3">
-                    <FiInfo size={100}/>
-                    <h3 class="text-3xl text-left text-white mt-8 ml-3">Status</h3>
+        <div className="space-y-28 ml-6 -mt-10">
+        <div className="grid grid-col-2  grid-flow-col backgroundSubCard m-3 gap-4 rounded-2xl" style={{width: "1000px", height: "100px"}}>
+                <div className="flex flex-wrap mt-2 ml-3">
+                        <FiInfo size={80} />
+                    <h3 class="text-3xl text-left text-white mt-5 ml-3">Status</h3>
                 </div>
         </div>
-        <div className="grid grid-col-2  grid-flow-col backgroundSubCard m-3 gap-4 rounded-lg" style={{width: "500px", height: "100px"}}>
+        <div className="grid grid-col-2  grid-flow-col backgroundSubCard m-3 gap-4 rounded-2xl" style={{width: "500px", height: "70px"}}>
             <div className="-ml-25 grid grid-col-2 grid-flow-col gap-x-2">
                 <div className="mt-3 inline grid grid-col-2 grid-flow-col">
-                    <h3 className="text-3xl text-left sfloat-left text-white m-4">http://localhost:3030</h3>
+                    <h3 className="text-3xl text-left sfloat-left text-white ml-4">http://localhost:3030</h3>
                 </div>
-                <div className="mt-4">
-                    <FiCopy size={60}/>
+                <div className="p-4 mt-2 rounded-2xl" style={{"background": "#6a3a6b", height: "50px", width: "60px", marginleft: "-80px"}}>
+                    <FiCopy style={{marginRight: "40px", marginLeft: "-5px", marginTop: "-10px"}} size={40} color="white"/>
                 </div>
             </div>
         </div>
-        <div className="float-left">
-          <Button className="p-3" buttonText="Delete Payment Link"/>
+        <div className="flex flex-col ml-3">
+        <button
+            className='text-center bg-red-600
+        text-1xl text-white font-bold p-3 w-56 h-15 rounded-xl'
+            >
+            Delete Payment Link
+        </button>        
+    </div>
+
         </div>
       </div>
     </div>
